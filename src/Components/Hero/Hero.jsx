d9a0a0b3766800e856330123f3e34bf37888af03
Abdul Khan <abdulkhan5355@gmail.com>
2024-11-19 20:53:25 +0530

    portfolio project created by anup yadav

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const Hero = () => {
+  return (
+    <div id="home" className="hero">
+      <img src={profile_img} alt="" />
+      <h1>
+        <span>I'm Alex Bennett,</span> frontend developer based in USA{" "}
+      </h1>
+      <p>
+        I am a ffrontend developer from california,USA with 10 years of
+        experience im multiple companies like Microsoft ,Tesla ans Apple{" "}
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me
+          </AnchorLink>
+        </div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
